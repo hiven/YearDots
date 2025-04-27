f954d3caa613c1149805a12044d29d89a6c7240f
hiven <mail@hiven.com>
2025-04-27 11:52:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://redsnips_user:5giaSr1exfKwJQ1krdhpmuMjAouydCmu@dpg-cp6e8do21fec738guak0-a.oregon-postgres.render.com/redsnips'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    
-    POSTS_PER_PAGE = 10
-    
-
-    
-    PREPROCESS_BUCKET = 'originalmedia'
-    POSTPROCESS_BUCKET = 'processedmedia'
-
-    PROCESSING_QUEUE = 'https://sqs.us-east-1.amazonaws.com/109633410385/ProcessObject.fifo'

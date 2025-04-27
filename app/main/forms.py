69458c1640afb1d8639dfab21dac138eaaf3cef5
hiven <mail@hiven.com>
2025-04-27 17:54:23 +0100

    Create forms.py

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, DateField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class AddHabitForm(FlaskForm):
+    name = StringField('Habit Name', validators=[DataRequired()])
+    submit = SubmitField('Add Habit')
+
+class AddActivityForm(FlaskForm):
+    habit_id = SelectField('Habit', coerce=int, validators=[DataRequired()])
+    date = DateField('Date', validators=[DataRequired()])
+    completed = BooleanField('Completed')
+    submit = SubmitField('Save Activity')

fb4870dcd6353c491bfdf03099472fad1e64361d
hiven <mail@hiven.com>
2025-05-03 22:11:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()                               # reads .env locally
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://yeardotsdb_user:m8UtCgbPCWyzBzVthRHq7WKBHGjHrVY2@dpg-d072bl2li9vc73esbh20-a/yeardotsdb'
+    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SECRET_KEY = getenv("SECRET_KEY", "dev-key")

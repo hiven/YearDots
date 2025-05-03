92b5a7cf4ab9de06d396bbc6dc3c9bfb1dd7aea6
hiven <mail@hiven.com>
2025-05-03 22:12:01 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+from config import Config
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
-db = SQLAlchemy(app)
+app.config.from_object(Config)
+
+db   = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 from app.main import main_bp
 app.register_blueprint(main_bp)
 
-# Create the database tables
 with app.app_context():
     db.create_all()

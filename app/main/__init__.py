2bbbf648ece600206786b3c387cc1d9fdacd20a3
hiven <mail@hiven.com>
2025-05-03 22:13:14 +0100

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
 
-main_bp = Blueprint('main', __name__, template_folder='templates')
-from app.main import routes
+main_bp = Blueprint("main", __name__, template_folder="templates", static_folder="static")
+
+from app.main import routes   # noqa: E402

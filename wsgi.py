f87f586c9a7d52e55686408d3b4e800678ba2308
hiven <mail@hiven.com>
2025-05-04 06:03:38 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from app import app
+from app import create_app
+
+app = create_app()          # the server only sees this one object
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

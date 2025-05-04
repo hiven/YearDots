25d79a780852532ef6752918a5b0a2c8f00f414d
hiven <mail@hiven.com>
2025-05-04 06:04:19 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
 
 from config import Config
 
@@ -8,7 +7,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db   = SQLAlchemy(app)
-csrf = CSRFProtect(app)
 
 from app.main import main_bp
 app.register_blueprint(main_bp)

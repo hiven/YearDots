d7b3ee638a06e112e68463a435f520ffa20c4e82
hiven <mail@hiven.com>
2025-05-05 07:20:41 +0100

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,16 +8,25 @@ from wtforms import (
     TextAreaField,
 )
 from wtforms.validators import DataRequired
+from flask import current_app
 
 
 class AddHabitForm(FlaskForm):
-    name = StringField("Habit Name", validators=[DataRequired()])
-    submit = SubmitField("Add Habit")
+    name   = StringField("Habit Name", validators=[DataRequired()])
+    colour = SelectField("Dot Colour", validators=[DataRequired()], coerce=str)
+    submit = SubmitField("Save Habit")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        palette = current_app.config["HABIT_COLOURS"]
+        self.colour.choices = [(c, c) for c in palette]
+        if not self.colour.data:
+            self.colour.data = palette[0]
 
 
 class AddActivityForm(FlaskForm):
-    habit_id = SelectField("Habit", coerce=int, validators=[DataRequired()])
-    date     = DateField("Date", validators=[DataRequired()])
+    habit_id  = SelectField("Habit", coerce=int, validators=[DataRequired()])
+    date      = DateField("Date", validators=[DataRequired()])
     completed = BooleanField("Completed")
     note      = TextAreaField("Note")
     submit    = SubmitField("Save Activity")
